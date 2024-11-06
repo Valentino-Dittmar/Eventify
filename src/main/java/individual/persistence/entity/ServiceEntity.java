eda15f248e4a515fa54ccbad2cc1c19931e4ab98
Valentino-Dittmar <151268060+Valentino-Dittmar@users.noreply.github.com>
2024-11-06 14:38:34 +0100

    Database set up and sonarqube property changes
    Changes

--- src/main/java/individual/persistence/entity/ServiceEntity.java
+++ src/main/java/individual/persistence/entity/ServiceEntity.java
@@ -1,12 +1,27 @@
 package individual.persistence.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@Entity
+@Table(name = "service")
+@AllArgsConstructor
+@NoArgsConstructor
 public class ServiceEntity {
-    public Long serviceId;
-    public String name;
-    public String description;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "service_id")
+    private Long serviceId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
 }
